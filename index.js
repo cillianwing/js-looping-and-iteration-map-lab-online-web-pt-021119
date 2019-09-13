1442b3b0169495962e98df60ac340d09e9618673
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-09-13 17:25:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function lowerCaseDrivers(arr) {
 
 function nameToAttributes(arr) {
   let newArray = arr.map(function(driver) {
-    splitArray = driver.split(" ");
+    let splitArray = driver.split(" ");
     first = splitArray[0];
     last = splitArray[1];
     return Object.assign({}, {firstName: first[0], lastName: last[0]})
