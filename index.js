3116e892204077a6950b274c8fcaa5c4f0003cc7
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-09-13 17:29:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function nameToAttributes(arr) {
 
 function attributesToPhrase(obj) {
   let newObject = obj.map(function(driver) {
-    return console.log(`${driver[name]} is from ${driver[hometown]}`);
+    return console.log(`${driver.name} is from ${driver.hometown}`);
   })
 }
