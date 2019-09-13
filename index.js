4665750f9625484b11cad5841c9ec5076508abf7
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-09-13 17:15:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file.
+function lowerCaseDrivers(arr) {
+  let newArray = arr.map(function(driver){
+    return driver.toLowerCase();
+  })
+  return newArray;
+}
+
+function nameToAttributes() {
+  
+}
